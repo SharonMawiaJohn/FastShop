2b2545bb8d3a8ef14e17dc350b16bdc7edc90511
SharonMawiaJohn <sharonmawia76@gmail.com>
2025-02-01 19:52:17 +0300

    Pushed changes made on my jsx files

--- src/components/ui/NavLink.jsx
+++ src/components/ui/NavLink.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 const NavLink = () => {
   return (
     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-            <a className="nav-link active fw-semibold" href="#!">Home</a>
-        </li>
-        <li className="nav-item">
-            <Link to="/profile" className="nav-link fw-semibold" href="#!">Shop</Link>
-        </li>
-        <li className="nav-item">
-            <Link className="nav-link fw-semibold" href="#!">About</Link>
-        </li>
-        <li className="nav-item">
-            <Link className="nav-link fw-semibold" href="#!">Contact</Link>
-        </li>
+      <li className="nav-item">
+        <a className="nav-link active fw-semibold" href="#!">Home</a>
+      </li>
+      <li className="nav-item">
+        <Link to="/profile" className="nav-link fw-semibold">Shop</Link>
+      </li>
+      <li className="nav-item">
+        <Link to="/about" className="nav-link fw-semibold">About</Link>
+      </li>
+      <li className="nav-item">
+        <Link to="/contact" className="nav-link fw-semibold">Contact</Link>
+      </li>
     </ul>
-  )
-}
+  );
+};
 
-export default NavLink
+export default NavLink;
