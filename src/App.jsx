624de2b56054aa359380bc4e4779494bd3f0f2ad
SharonMawiaJohn <sharonmawia76@gmail.com>
2025-02-09 00:11:11 +0300

    Adds the useEffect on product_cart

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,27 @@ import HomePage from "./components/home/HomePage";
 import MainLayout from "./components/layout/MainLayout";
 import NotFoundPage from './components/ui/NotFoundPage';
 import ProductPage from './components/product/ProductPage';
-import {useState} from 'react'
+import {useState, useEffect} from 'react';
+import api from "./api";
 
 const App = () => {
 
   const [numCartItems, setNumberCartItems] = useState(0);
+  const cart_code = localStorage.getItem("cart_code")
+
+  useEffect(function(){
+    if(cart_code){
+      api.get(`get_cart_stat?cart_code=${cart_code}`)
+      .then(res=>{
+        console.log(res.data)
+      })
+
+      .catch(err => {
+        console.log(err.message)
+      })
+    }
+    
+  })
 
   return (
     <BrowserRouter>
